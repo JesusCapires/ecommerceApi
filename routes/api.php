0743645b04cfe8023146a54e59779bba1ddf3765
JesusCapires <capa.2901@gmail.com>
2024-07-31 17:58:35 -0600

    Primer commit, configuracion

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:api');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh')->middleware('auth:api');
+    Route::post('/me', [AuthController::class, 'me'])->name('me')->middleware('auth:api');
+});
